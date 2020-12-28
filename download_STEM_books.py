39c89b85538a17096ce1ac34791e13769bf4525f
daphtd <44963656+daph-td@users.noreply.github.com>
2020-12-28 13:20:31 +0700

    Add files via upload

---
+++ download_STEM_books.py
@@ -0,0 +1,16 @@
+import requests, wget
+import pandas as pd
+df = pd.read_excel("Free+English+textbooks.xlsx")
+# print(df.head(10))
+
+for index, row in df.iterrows():
+        # loop through the excel list
+        file_name = f"{row.loc['Book Title']}_{row.loc['Edition']}".replace('/','-').replace(':','-')
+        #print(file_name)
+        url = f"{row.loc['OpenURL']}"
+        #print(url)
+        r = requests.get(url) 
+        download_url = f"{r.url.replace('book','content/pdf')}.pdf"
+        #print(download_url)
+        wget.download(download_url, f"./download/{file_name}.pdf") 
+        print(f"downloading {file_name}.pdf Complete ....")
